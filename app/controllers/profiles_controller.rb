46e6b7acbc82e531ce683b9066d39ec4b8a446a7
Jeremie Rykner <jrykner@gmail.com>
2016-11-19 19:02:49 +0000

    Added users show action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -14,8 +14,23 @@ class ProfilesController < ApplicationController
       render action: :new
     end
   end
+  def edit
+  @user = User.find( params[:user_id] )
+  @profile = @user.profile
+  end
   private
     def profile_params
       params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
     end
+ 
+  def update
+    @user = User.find( params[:user_id] )
+    @profile = @user.profile
+    if @profile.update_attributes(profile_params)
+    flash[:success] = "Profile Updated!"
+    redirect_to user_path( params[:user_id] )
+    else
+    render action: :edit
+    end
+  end
 end
